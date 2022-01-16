3a41ec156b93173b9776a2e857deaac6d9c9959d
Dmitry Vdovin <voidxnull@gmail.com>
2022-01-16 13:37:56 +0400

    Fix compile errors

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,7 +41,7 @@ pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use zeropool_pallet;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -275,7 +275,7 @@ impl pallet_sudo::Config for Runtime {
 }
 
 /// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl zeropool_pallet::Config for Runtime {
 	type Event = Event;
 }
 
@@ -295,7 +295,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		TemplateModule: zeropool_pallet,
 	}
 );
 
@@ -474,7 +474,7 @@ impl_runtime_apis! {
 			list_benchmark!(list, extra, frame_system, SystemBench::<Runtime>);
 			list_benchmark!(list, extra, pallet_balances, Balances);
 			list_benchmark!(list, extra, pallet_timestamp, Timestamp);
-			list_benchmark!(list, extra, pallet_template, TemplateModule);
+			list_benchmark!(list, extra, zeropool_pallet, TemplateModule);
 
 			let storage_info = AllPalletsWithSystem::storage_info();
 
@@ -512,7 +512,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
 			add_benchmark!(params, batches, pallet_balances, Balances);
 			add_benchmark!(params, batches, pallet_timestamp, Timestamp);
-			add_benchmark!(params, batches, pallet_template, TemplateModule);
+			add_benchmark!(params, batches, zeropool_pallet, TemplateModule);
 
 			Ok(batches)
 		}
