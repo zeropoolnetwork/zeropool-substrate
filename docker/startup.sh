ab284f3bf4b12820bdda7ae761cca2077bdb5226
Dmitry Vdovin <voidxnull@gmail.com>
2022-03-16 19:45:15 +0400

    Commit fix startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,5 +3,5 @@
 export RPC_URL=ws://127.0.0.1:9944
 export PRIVATE_KEY=$RELAYER_PRIVATE_KEY
 
-./node-template --dev --ws-external &
-node init.js && fg
+node init.js &
+./node-template --dev --ws-external
