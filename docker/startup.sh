8b20155025b0c4daeb6cd60409522bcd2c549190
Dmitry Vdovin <voidxnull@gmail.com>
2022-04-19 17:36:27 +0400

    Fix scripts

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,4 +4,5 @@ export RPC_URL=ws://127.0.0.1:9944
 export PRIVATE_KEY=$RELAYER_PRIVATE_KEY
 
 node init.js &
-./node-template --dev --ws-external
+
+./node-template --dev --ws-external --base-path /tmp/substrate
